21026387f11aa4d22a124403a93747ea2e794739
Lucille Wang <lucillewang@Lucilles-MacBook-Pro.local>
2017-01-31 13:22:25 -0500

    Modified Person4.java and AntLabMain.java

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,14 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+        //Person 4 put your implementation here
+        String result = "";
+        for (int i = 0; i < input.length(); i++) {
+            int newAscii = (int) input.charAt(i) + 1;
+            char newChar = (char) newAscii;
+            result = result + newChar;
+        }
+        return result;
     }
     
     /**
